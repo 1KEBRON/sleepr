27350ebcd717e1f6bbef660e8e9a3e3dab2b7b75
kebron solomon zerie <kebron1832@hotmail.com>
2021-08-07 22:56:58 +0200

    add bootstrap , add elements to alarm page

---
+++ script.js
@@ -0,0 +1,4 @@
+
+1.build a bedtime function that takes in  a date as an input and returns by counting forward in (90) + 15 min cycles,
+ranging from 1-6 cycles
+2. build a wakeuptime function that invert the bedtime function  
